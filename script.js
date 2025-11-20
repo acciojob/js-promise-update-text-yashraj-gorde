409e2308b4ebb5efe6c86d4e6eb530ae1324de38
Yashraaj Sharad Gorde <149304566+yashraj-gorde@users.noreply.github.com>
2025-11-20 07:35:58 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
 //your JS code here. If required.
+function helloPromise() {
+  return new Promise((resolve) => {
+    setTimeout(() => {
+      resolve("Hello, world!");
+    }, 1000); // 1 second
+  });
+}
+
+helloPromise().then((message) => {
+  document.getElementById("output").innerText = message;
+});
